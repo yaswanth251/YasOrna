73a261202802e3d8c59ec7029b00e12636cf5fb3
K Yaswanth kumar <135256798+yaswanth251@users.noreply.github.com>
2025-04-05 10:08:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
+  base: './',  // Add this line to resolve module paths correctly
 });
